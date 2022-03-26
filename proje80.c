60c3e54bdb58f442656761bf07b353a90c643af6
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:52:04 +0300

    sum

---
+++ proje80.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	char kelime[400];
+	
+	printf("Kelime Girin: ");
+	scanf("%s",kelime);
+	
+	printf("Katar Uzunlugu: %d",strlen(kelime));
+	
+	
+	
+	
+	
+	//***************************************************
+	
+	//char kitap[40];
+	
+	//printf("Kitap Ad: ");
+	//scanf("%s",&kitap);
+	
+	//printf("%s",kitap);
+	
+	//printf("\n");
+	
+	//printf("%15s",kitap);
+	
+	//printf("\n");
+	
+	//printf("%20.5s",kitap);
+	
+	//printf("\n");
+	
+	//printf("%-20s",kitap);
+	
+	
+	//***************************************************
+	
+	
+	//char bilgi[40];
+	
+	//printf("Bilgi Girin: ");
+	//gets(bilgi);
+	
+	//printf("\n\n");
+	
+	//puts(bilgi);
+	
+	return 0;
+}
